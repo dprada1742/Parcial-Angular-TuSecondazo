c2ab4aab2200dd0797adc5da7d18391a9df4a1b1
daniela <d.prada1742@uniandes.edu.co>
2023-04-28 16:30:48 -0500

    Add: Creacion de unit tests

--- src/app/vehiculo/vehiculo-list/vehiculo-list.component.spec.ts
+++ src/app/vehiculo/vehiculo-list/vehiculo-list.component.spec.ts
@@ -2,27 +2,59 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 import { VehiculoListComponent } from './vehiculo-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { Vehiculo } from '../vehiculo';
+import { VehiculoService } from '../vehiculo.service';
 
 describe('VehiculoListComponent', () => {
   let component: VehiculoListComponent;
   let fixture: ComponentFixture<VehiculoListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ VehiculoListComponent ]
-    })
-    .compileComponents();
+      imports: [HttpClientModule],
+      declarations: [VehiculoListComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(VehiculoListComponent);
     component = fixture.componentInstance;
+
+    for (let i = 0; i < 3; i++) {
+      const vehiculo = new Vehiculo(
+        faker.datatype.number(),
+        faker.vehicle.manufacturer(),
+        faker.vehicle.type(),
+        faker.vehicle.model(),
+        faker.datatype.number(),
+        faker.datatype.number(),
+        faker.lorem.word(),
+        faker.image.imageUrl()
+      );
+
+      component.vehiculos.push(vehiculo);
+    }
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have the table header', () => {
+    const headerRow = fixture.debugElement.query(By.css('thead tr'));
+    expect(headerRow).toBeTruthy();
+  });
+
+  it('should display 3 rows', () => {
+    const rows = fixture.debugElement.queryAll(By.css('tbody tr'));
+    expect(rows.length).toBe(3);
   });
 });
